dffa22f298d80b7fd2c1f6f12aa19a9cd57aa2d3
Gowri <ivcheero@gmail.com>
2017-07-11 15:09:16 +0530

    Update 1000 digit Fibonacci number.py
    Changed counter = 0 to counter = 1 because sensibly the sequence starts from F1 not F0

--- 1000 digit Fibonacci number.py
+++ 1000 digit Fibonacci number.py
@@ -2,7 +2,7 @@ n1 = 0
 n2 = 1
 n3 = 0
 g1 = 0
-counter = 0
+counter = 1
 def even_fib():
 	global n1,n2,n3,g1,counter
 	for i in range(1,10000):
@@ -13,4 +13,4 @@ def even_fib():
 		if len(str(n3)) == 1000:
 			break
 even_fib()
-print('The first term in the Fibonacci sequence to contain 1000 digits is ' + str(counter+1))
+print('The first term in the Fibonacci sequence to contain 1000 digits is ' + str(counter))
