a19a5124f8c257483ea5af846c260113bd00981d
Gowri <ivcheero@gmail.com>
2017-07-11 15:02:28 +0530

    Finds the term to have 1000 digits
    The Fibonacci sequence is defined by the recurrence relation:
    
    Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
    Hence the first 12 terms will be:
    
    F1 = 1
    F2 = 1
    F3 = 2
    F4 = 3
    F5 = 5
    F6 = 8
    F7 = 13
    F8 = 21
    F9 = 34
    F10 = 55
    F11 = 89
    F12 = 144
    The 12th term, F12, is the first term to contain three digits.
    
    What is the index of the first term in the Fibonacci sequence to contain 1000 digits?

---
+++ 1000 digit Fibonacci number.py
@@ -0,0 +1,16 @@
+n1 = 0
+n2 = 1
+n3 = 0
+g1 = 0
+counter = 0
+def even_fib():
+	global n1,n2,n3,g1,counter
+	for i in range(1,10000):
+		counter+= 1
+		n3 = n1 + n2
+		n1 = n2
+		n2 = n3
+		if len(str(n3)) == 1000:
+			break
+even_fib()
+print('The first term in the Fibonacci sequence to contain 1000 digits is ' + str(counter+1))
